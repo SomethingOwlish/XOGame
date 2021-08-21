13ee580d657781ea26a8a1f136e8750738da0e1f
SomethingOwlish <55540709+SomethingOwlish@users.noreply.github.com>
2021-08-22 00:52:28 +0300

    change field size

--- main.cpp
+++ main.cpp
@@ -12,34 +12,37 @@ struct Game{
  bool isStart;
     std::string namePL;
     std::string namePL2;
+    int fieldSizer;
 };
 
 int main() {
  Game* game = new Game;
  game->isStart = true;
-
-
-              srand(time(0));
+  srand(time(0));
   std::cout <<  "Привет! Рад что ты заглянул поиграть со мной в крестики-нолики!" << std::endl;
     std::string one = "имя  игрока";
+   // std::string fieldSize = "сколько клеток?";
     game->namePL = askSmthg(one);
+  //  std::cout <<  "Размер поля какой будет?" << std::endl;
+    game->fieldSizer = 3;
+
     char **list;
-    list = new char *[3];
-    for (size_t i = 0; i < 3; i++) {
-        list[i] = new char[3];
+    list = new char *[game->fieldSizer];
+    for (size_t i = 0; i < game->fieldSizer; i++) {
+        list[i] = new char[game->fieldSizer];
     };
-    for (int j = 0; j < 3; j++) {
-        for (int k = 0; k < 3; k++) {
+    for (int j = 0; j < game->fieldSizer; j++) {
+        for (int k = 0; k < game->fieldSizer; k++) {
             list[j][k] ='E';}};
 
   game->field = *list;
 
     if(choseStyle()){
-    gameForOne(list, 3, 3, game->countMove, game->namePL);}
+    gameForOne(list, game->fieldSizer, game->fieldSizer, game->countMove, game->namePL);}
     else {
         std::string two= "имя второго игрока";
         game->namePL2 = askSmthg(two);
-        gameForTwo(list, 3, 3, game->countMove, game->namePL, game->namePL2);};
+        gameForTwo(list, game->fieldSizer, game->fieldSizer, game->countMove, game->namePL, game->namePL2);};
 
     game->isStart = false;
     for (size_t i = 0; i < 3; i++) {
