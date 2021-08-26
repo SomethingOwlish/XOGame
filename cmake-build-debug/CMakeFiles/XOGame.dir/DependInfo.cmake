d575100a1a8056814de2c9a3f1f2ece88483925d
SomethingOwlish <55540709+SomethingOwlish@users.noreply.github.com>
2021-08-26 11:26:40 +0300

    ап

---
+++ cmake-build-debug/CMakeFiles/XOGame.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/morganvalravn/Documents/Projects/XOGame/gameForOne.cpp" "/Users/morganvalravn/Documents/Projects/XOGame/cmake-build-debug/CMakeFiles/XOGame.dir/gameForOne.cpp.o"
+  "/Users/morganvalravn/Documents/Projects/XOGame/ganeForTwo.cpp" "/Users/morganvalravn/Documents/Projects/XOGame/cmake-build-debug/CMakeFiles/XOGame.dir/ganeForTwo.cpp.o"
+  "/Users/morganvalravn/Documents/Projects/XOGame/main.cpp" "/Users/morganvalravn/Documents/Projects/XOGame/cmake-build-debug/CMakeFiles/XOGame.dir/main.cpp.o"
+  "/Users/morganvalravn/Documents/Projects/XOGame/source.cpp" "/Users/morganvalravn/Documents/Projects/XOGame/cmake-build-debug/CMakeFiles/XOGame.dir/source.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/XOGame.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/XOGame.dir/gameForOne.cpp.o"
+  "CMakeFiles/XOGame.dir/ganeForTwo.cpp.o"
+  "CMakeFiles/XOGame.dir/main.cpp.o"
+  "CMakeFiles/XOGame.dir/source.cpp.o"
+  "XOGame"
+  "XOGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/XOGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
